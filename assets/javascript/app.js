60948e28c8cd2de93f283801724fc569f52697c0
Devin Baran <TheResistance@wifi_stu-10-38-91-77.uncc.edu>
2018-04-14 09:41:58 -0400

    trivia

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,32 @@
+var total = 5;
+var score = 0;
+
+window.onload = function () {
+    var oneMinute = 25 * 1,
+        display = document.querySelector('#time');
+    startTimer(oneMinute, display);
+
+    function startTimer(duration, display) {
+        var timer = duration, minutes, seconds;
+        setInterval(function () {
+            minutes = parseInt(timer / 60, 10)
+            seconds = parseInt(timer % 60, 10);
+    
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+    
+            display.textContent = minutes + ":" + seconds;
+    
+            if (--timer < 0) {
+                timer = 0;
+                alert('Times up. Try again')
+                window.location.reload();
+            }
+        }, 1000);
+    }
+};
+
 function submitAnswers () {
-    var total = 5;
-    var score = 0;
 
     //Get user input//
     var q1 = document.forms['questionForm']['q1'].value;
@@ -10,7 +36,6 @@ function submitAnswers () {
     var q5 = document.forms['questionForm']['q5'].value;
 
     //validation//
-    
     for(var i = 1; i <= total; i++){
         if(eval('q'+i) == null || eval('q'+i) == '') {
             alert('You missed question '+ i);
@@ -19,5 +44,16 @@ function submitAnswers () {
     }
 
     //set correct answers//
-    var answers = ['d','c','d','d','d'];
+    var answers = ['d','a','d','d','d'];
+
+    //Check answers//
+    for(var i = 1; i <= total; i++) {
+        if(eval('q'+i) == answers[i - 1]) {
+            score++;
+        }
+    }
+
+    //Display results//
+    alert('You scored ' + score + ' out of ' + total);
+    window.location.reload();
 }
